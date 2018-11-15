66814d7017ce636debc9663404bdd791ede136ee
Colin Millar <colinpmillar@gmail.com>
2018-11-15 22:59:25 +0100

    script for plotting sar

---
+++ report_abrasion.R
@@ -0,0 +1,95 @@
+## Prepare plots/tables for report
+
+## Before:
+## After:
+
+# libraries
+#install_github("ices-tools-dev/sfdMaps")
+#library(sfdMaps)
+devtools::load_all("../../ices-tools-dev/sfdMaps/")
+library(icesTAF)
+library(dplyr)
+library(glue)
+library(jsonlite)
+
+# read in config
+config <- read_json("config.json")
+
+# read in data
+vms <- read.taf("data/fishing_activity.csv")
+ecoregion_table <- read.taf("bootstrap/data/ecoregion_table.csv")
+
+# get ecoregion name
+ecoregion_name <-
+  ecoregion_table$Description[ecoregion_table$Key == config$ecoregion]
+
+# read in config
+config <- read_json("config.json")
+
+# read in data
+vms <- read.taf("data/fishing_activity.csv")
+ecoregion_table <- read.taf("bootstrap/data/ecoregion_table.csv")
+
+# get ecoregion name
+ecoregion_name <-
+  ecoregion_table$Description[ecoregion_table$Key == config$ecoregion]
+
+# surface abrasion map plot
+
+# sum up over all gears
+vms_sub <-
+  vms %>%
+    mutate(
+      area = csquare_area(c_square)
+    ) %>%
+    group_by(c_square, year) %>%
+      summarise(
+        surface_sar = sum(surface / area, na.rm = TRUE),
+        subsurface_sar = sum(subsurface / area, na.rm = TRUE)
+      ) %>%
+    ungroup() %>%
+    group_by(c_square) %>%
+    summarise(
+      surface_sar = mean(surface_sar, na.rm = TRUE),
+      subsurface_sar = mean(subsurface_sar, na.rm = TRUE)
+    ) %>%
+  ungroup() %>%
+    mutate(
+      lat = sfdSAR::csquare_lat(c_square),
+      lon = sfdSAR::csquare_lon(c_square)
+    )
+
+# make an all feild
+vms_sub$all_gears <- "all"
+
+png("report/surface_sar.png",
+    width = 8.4, height = 8.4,
+    res = 400, units = "cm", pointsize = 5)
+
+plotPages(vms_sub$surface_sar,
+          vms_sub[c("lon", "lat")],
+          vms_sub$all_gears,
+          ecoregion,
+          glue("Average surface swept area ratio {config$year - 3}-{config$year}"),
+          "Surface SweptArea Ratio",
+          c("all" = "All gears"),
+          breaks = c(0, 0.5, 1, 2, 5, 10, 20, 50, Inf),
+          digits = 1)
+
+dev.off()
+
+png("report/subsurface_sar.png",
+    width = 8.4, height = 8.4,
+    res = 400, units = "cm", pointsize = 5)
+
+plotPages(vms_sub$subsurface_sar,
+          vms_sub[c("lon", "lat")],
+          vms_sub$all_gears,
+          ecoregion,
+          glue("Average subsurface swept area ratio {config$year - 3}-{config$year}"),
+          "Subsurface SweptArea Ratio",
+          c("all" = "All gears"),
+          breaks = c(0, 0.5, 1, 2, 5, 10, 20, 50, Inf) / 10,
+          digits = 2)
+
+dev.off()
